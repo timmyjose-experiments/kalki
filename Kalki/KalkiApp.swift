2893bcd56f7cab67b348d2fe21882a4dc705f255
Timmy Jose <zoltan.jose@gmail.com>
2024-12-17 21:53:43 +0530

    Initial Commit

---
+++ Kalki/KalkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalkiApp.swift
+//  Kalki
+//
+//  Created by Timmy Jose on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct KalkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KalkiTests/KalkiTests.swift
@@ -0,0 +1,17 @@
+//
+//  KalkiTests.swift
+//  KalkiTests
+//
+//  Created by Timmy Jose on 17/12/24.
+//
+
+import Testing
+@testable import Kalki
+
+struct KalkiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
